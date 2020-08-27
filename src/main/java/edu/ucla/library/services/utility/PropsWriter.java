25133ca340f13e0e04fc9dd0ae7ecb57588c21ac
David Rickard <drickard1967@library.ucla.edu>
2020-08-27 18:50:09 +0000

    style fixes

--- src/main/java/edu/ucla/library/services/utility/PropsWriter.java
+++ src/main/java/edu/ucla/library/services/utility/PropsWriter.java
@@ -13,147 +13,153 @@ import java.util.Properties;
 
 public final class PropsWriter {
 
-  /**
-   * Properties file used as source for output file.
-  */
-  private static final Properties PROPS = new Properties();
+    /**
+     * Properties file used as source for output file.
+    */
+    private static final Properties PROPS = new Properties();
 
-  /**
-   * Expected number of parameters passed to class.
-  */
-  private static final int ARGS_COUNT = 3;
+    /**
+     * Expected number of parameters passed to class.
+    */
+    private static final int ARGS_COUNT = 3;
 
-  /**
-   * Private constructor for PropsWriter class.
-  */
-  private PropsWriter() {
-    super();
-  }
+    /**
+     * Delimiter character for value keys in template file.
+    */
+    private static final String DELIMITER = "$";
 
-  /**
-   * Main method for command-line execution.
-   *
-   * @param args array of parameters
-  */
-  public static void main(final String[] args) {
-    verifyArgs(args);
-    loadProps(args[0]);
-    readWrite(args[1], args[2]);
-  }
-
-  /**
-   * Public method for testing access or plugging into other classes.
-   *
-   * @param aDefaults default values fed to PROPS
-   * @param aTemplate template for the output file
-   * @param aOutput properties file built by app from aDefaults and aTemplate
-  */
-  public static void buildProperties(final String aDefaults,
-    final String aTemplate, final String aOutput) {
-    verifyArgs(new String[] {aDefaults, aTemplate, aOutput});
-    loadProps(aDefaults);
-    readWrite(aTemplate, aOutput);
-  }
-
-  /**
-   * Check that parameters are valid.
-   *
-   * @param aArray array of file names
-  */
-  public static void verifyArgs(final String[] aArray) {
-    if (aArray.length != ARGS_COUNT) {
-      System.err.println(
-        "usage: PropsWriter defaults template output");
-      System.exit(Constants.USAGE_ERROR);
+    /**
+     * Private constructor for PropsWriter class.
+    */
+    private PropsWriter() {
+        super();
     }
-    // check that default props and template files exist/are readable
-    checkExists(aArray[0]);
-    checkReadable(aArray[0]);
-    checkExists(aArray[1]);
-    checkReadable(aArray[1]);
-  }
 
-  /**
-   * Verify a file exists.
-   *
-   * @param aFileName pathed name of file to test
-  */
-  public static void checkExists(final String aFileName) {
-    if (!Files.exists(FileSystems.getDefault().getPath(aFileName))) {
-      System.err.println("File must exist: " + aFileName);
-      System.exit(Constants.DOESNT_EXIST_ERROR);
+    /**
+     * Main method for command-line execution.
+     *
+     * @param args array of parameters
+    */
+    @SuppressWarnings("uncommentedmain")
+    public static void main(final String[] args) {
+        verifyArgs(args);
+        loadProps(args[0]);
+        readWrite(args[1], args[2]);
     }
-  }
 
-  /**
-   * Verify a file is readable.
-   *
-   * @param aFileName pathed name of file to test
-  */
-  public static void checkReadable(final String aFileName) {
-    if (!Files.isReadable(FileSystems.getDefault().getPath(aFileName))) {
-      System.err.println("File must be readable: " + aFileName);
-      System.exit(Constants.NOT_READABLE_ERROR);
+    /**
+     * Public method for testing access or plugging into other classes.
+     *
+     * @param aDefaults default values fed to PROPS
+     * @param aTemplate template for the output file
+     * @param aOutput properties file built by app from aDefaults and aTemplate
+    */
+    public static void buildProperties(final String aDefaults,
+        final String aTemplate, final String aOutput) {
+        verifyArgs(new String[] {aDefaults, aTemplate, aOutput});
+        loadProps(aDefaults);
+        readWrite(aTemplate, aOutput);
     }
-  }
 
-  /**
-   * Load default properties into PROPS.
-   *
-   * @param aFileName pathed name of default props file
-  */
-  public static void loadProps(final String aFileName) {
-    try {
-      PROPS.load(new FileInputStream(new File(aFileName)));
-    } catch (FileNotFoundException e) {
-      System.err.println("Props file must exist: "
-        + e.getMessage());
-      System.exit(Constants.DOESNT_EXIST_ERROR);
-    } catch (IOException details) {
-      System.err.println("I/O error reading props file: "
-        + details.getMessage());
-      System.exit(Constants.IO_ERROR);
-    }
-  }
-
-  /**
-   * Splice default properties in template and write to output file.
-   *
-   * @param aInput pathed name of template file
-   * @param aOutput pathed name of final properties file
-  */
-  public static void readWrite(final String aInput, final String aOutput) {
-    final BufferedReader reader;
-    final BufferedWriter writer;
-    String line = null;
-    try {
-      reader = Files.newBufferedReader(
-        FileSystems.getDefault().getPath(aInput));
-      writer = Files.newBufferedWriter(
-        FileSystems.getDefault().getPath(aOutput));
-      while ((line = reader.readLine()) != null) {
-        if (line.contains("$")) {
-          String key = line.substring(line.indexOf("$") + 1);
-          writer.write(line.replace("$", "").replace(key,
-            PROPS.getProperty(key)));
-        } else {
-          writer.write(line);
+    /**
+     * Check that parameters are valid.
+     *
+     * @param aArray array of file names
+    */
+    public static void verifyArgs(final String[] aArray) {
+        if (aArray.length != ARGS_COUNT) {
+            System.err.println(
+                "usage: PropsWriter defaults template output");
+            System.exit(Constants.USAGE_ERROR);
         }
-        writer.newLine();
-      }
-      reader.close();
-      writer.newLine();
-      writer.flush();
-      writer.close();
-    } catch (FileNotFoundException details) {
-      System.err.println("Problem finding a file: "
-        + details.getMessage());
-      System.exit(Constants.DOESNT_EXIST_ERROR);
-    } catch (IOException details) {
-      System.err.println("I/O problem with a file: "
-        + details.getMessage());
-      System.exit(Constants.IO_ERROR);
+      // check that default props and template files exist/are readable
+        checkExists(aArray[0]);
+        checkReadable(aArray[0]);
+        checkExists(aArray[1]);
+        checkReadable(aArray[1]);
     }
-  }
+
+    /**
+     * Verify a file exists.
+     *
+     * @param aFileName pathed name of file to test
+    */
+    public static void checkExists(final String aFileName) {
+        if (!Files.exists(FileSystems.getDefault().getPath(aFileName))) {
+            System.err.println("File must exist: " + aFileName);
+            System.exit(Constants.DOESNT_EXIST_ERROR);
+        }
+    }
+
+    /**
+     * Verify a file is readable.
+     *
+     * @param aFileName pathed name of file to test
+    */
+    public static void checkReadable(final String aFileName) {
+        if (!Files.isReadable(FileSystems.getDefault().getPath(aFileName))) {
+            System.err.println("File must be readable: " + aFileName);
+            System.exit(Constants.NOT_READABLE_ERROR);
+        }
+    }
+
+    /**
+     * Load default properties into PROPS.
+     *
+     * @param aFileName pathed name of default props file
+    */
+    public static void loadProps(final String aFileName) {
+        try {
+            PROPS.load(new FileInputStream(new File(aFileName)));
+        } catch (FileNotFoundException e) {
+            System.err.println("Props file must exist: "
+                + e.getMessage());
+            System.exit(Constants.DOESNT_EXIST_ERROR);
+        } catch (IOException details) {
+            System.err.println("I/O error reading props file: "
+                + details.getMessage());
+            System.exit(Constants.IO_ERROR);
+        }
+    }
+
+    /**
+     * Splice default properties in template and write to output file.
+     *
+     * @param aInput pathed name of template file
+     * @param aOutput pathed name of final properties file
+    */
+    public static void readWrite(final String aInput, final String aOutput) {
+        final BufferedReader reader;
+        final BufferedWriter writer;
+        String line = null;
+        try {
+            reader = Files.newBufferedReader(
+                FileSystems.getDefault().getPath(aInput));
+            writer = Files.newBufferedWriter(
+                FileSystems.getDefault().getPath(aOutput));
+            while ((line = reader.readLine()) != null) {
+                if (line.contains(DELIMITER)) {
+                    final String key = line.substring(line.indexOf(DELIMITER) + 1);
+                    writer.write(line.replace(DELIMITER, "").replace(key,
+                        PROPS.getProperty(key)));
+                } else {
+                    writer.write(line);
+                }
+                writer.newLine();
+            }
+            reader.close();
+            writer.newLine();
+            writer.flush();
+            writer.close();
+        } catch (FileNotFoundException details) {
+            System.err.println("Problem finding a file: "
+                + details.getMessage());
+            System.exit(Constants.DOESNT_EXIST_ERROR);
+        } catch (IOException details) {
+            System.err.println("I/O problem with a file: "
+                + details.getMessage());
+            System.exit(Constants.IO_ERROR);
+        }
+    }
 
 }
--- src/test/java/edu/ucla/library/services/utility/PropsWriterTest.java
+++ src/test/java/edu/ucla/library/services/utility/PropsWriterTest.java
@@ -23,104 +23,104 @@ public class PropsWriterTest {
   /**
    * default properties file.
   */
-  private static final String DEFAULTS =
-    "src/test/resources/properties.default";
+    private static final String DEFAULTS =
+        "src/test/resources/properties.default";
 
   /**
    * output template file.
   */
-  private static final String TEMPLATE =
-    "src/test/resources/properties.tmpl";
+    private static final String TEMPLATE =
+        "src/test/resources/properties.tmpl";
 
   /**
    * final output file.
   */
-  private static final String OUTPUT =
-    "src/test/resources/output.properties";
+    private static final String OUTPUT =
+        "src/test/resources/output.properties";
 
   /**
    * fake file name used to cause app failures.
   */
-  private static final String FAKE =
-    "src/test/resources/not.there";
+    private static final String FAKE =
+        "src/test/resources/not.there";
 
   /**
    * rule that allows catching System.err output in tests.
   */
-  @Rule
-  public final SystemErrRule systemErrRule = new SystemErrRule().enableLog();
+    @Rule
+    public final SystemErrRule mySystemErrRule = new SystemErrRule().enableLog();
 
   /**
    * Sets up a PropsWriter test.
    *
    * @throws IOException If there is a problem deleting the file
   */
-  @Before
-  public void setUp() throws IOException {
-    // delete the output file if lest over from previous run
-    Files.deleteIfExists(FileSystems.getDefault().getPath(OUTPUT));
-  }
+    @Before
+    public void setUp() throws IOException {
+        // delete the output file if lest over from previous run
+        Files.deleteIfExists(FileSystems.getDefault().getPath(OUTPUT));
+    }
 
   /**
    * Tests happy path for app execution.
    *
   */
-  @Test
-  public void testBuildProperties() {
-    PropsWriter.buildProperties(DEFAULTS, TEMPLATE, OUTPUT);
-    assertTrue(Files.exists(FileSystems.getDefault().getPath(OUTPUT)));
-  }
+    @Test
+    public void testBuildProperties() {
+        PropsWriter.buildProperties(DEFAULTS, TEMPLATE, OUTPUT);
+        assertTrue(Files.exists(FileSystems.getDefault().getPath(OUTPUT)));
+    }
 
   /**
    * Tests that app catches nonexistent files.
    *
   */
-  @Test
-  public void testCheckExists() throws Exception {
-    int statusCode = catchSystemExit(() -> {
-      PropsWriter.checkExists(FAKE);
-    });
-    assertTrue(systemErrRule.getLog().contains("File must exist"));
-    assertEquals(Constants.DOESNT_EXIST_ERROR, statusCode);
-  }
+    @Test
+    public void testCheckExists() throws Exception {
+        final int statusCode = catchSystemExit(() -> {
+            PropsWriter.checkExists(FAKE);
+        });
+        assertTrue(mySystemErrRule.getLog().contains("File must exist"));
+        assertEquals(Constants.DOESNT_EXIST_ERROR, statusCode);
+    }
 
   /**
    * Tests that app catches nonreadable files.
    *
   */
-  @Test
-  public void testCheckReadable() throws Exception {
-    int statusCode = catchSystemExit(() -> {
-      PropsWriter.checkReadable(FAKE);
-    });
-    assertTrue(systemErrRule.getLog().contains("File must be readable"));
-    assertEquals(Constants.NOT_READABLE_ERROR, statusCode);
-  }
+    @Test
+    public void testCheckReadable() throws Exception {
+        final int statusCode = catchSystemExit(() -> {
+            PropsWriter.checkReadable(FAKE);
+        });
+        assertTrue(mySystemErrRule.getLog().contains("File must be readable"));
+        assertEquals(Constants.NOT_READABLE_ERROR, statusCode);
+    }
 
   /**
    * Tests that app catches missing parameters.
    *
   */
-  @Test
-  public void testVerifyArgs() throws Exception {
-    int statusCode = catchSystemExit(() -> {
-      PropsWriter.verifyArgs(new String[] {});
-    });
-    assertTrue(systemErrRule.getLog().contains("usage"));
-    assertEquals(Constants.USAGE_ERROR, statusCode);
-  }
+    @Test
+    public void testVerifyArgs() throws Exception {
+        final int statusCode = catchSystemExit(() -> {
+            PropsWriter.verifyArgs(new String[] {});
+        });
+        assertTrue(mySystemErrRule.getLog().contains("usage"));
+        assertEquals(Constants.USAGE_ERROR, statusCode);
+    }
 
   /**
    * Tests app catches nonexistent default props file.
    *
   */
-  @Test
-  public void testLoadProps() throws Exception {
-    int statusCode = catchSystemExit(() -> {
-      PropsWriter.loadProps(FAKE);
-    });
-    assertTrue(systemErrRule.getLog().contains("Props file must exist"));
-    assertEquals(Constants.DOESNT_EXIST_ERROR, statusCode);
-  }
+    @Test
+    public void testLoadProps() throws Exception {
+        final int statusCode = catchSystemExit(() -> {
+            PropsWriter.loadProps(FAKE);
+        });
+        assertTrue(mySystemErrRule.getLog().contains("Props file must exist"));
+        assertEquals(Constants.DOESNT_EXIST_ERROR, statusCode);
+    }
 
 }
